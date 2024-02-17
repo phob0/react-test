80a3e6118ff02e02306eb758248fc3ce791aa071
Alex <ph0bo@protonmail.com>
2024-02-17 16:20:03 +0200

    responsive grid to listing

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -3,6 +3,10 @@ import _ from 'lodash';
 import Book from './Book';
 import BooksContext from '../context/BooksContext';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 const BooksList = () => {
 
   const { books, setBooks } = useContext(BooksContext);
@@ -11,17 +15,28 @@ const BooksList = () => {
     setBooks(books.filter((book) => book.id !== id));
   };
 
+  function splitArrayIntoChunks(array, chunkSize = 3) {
+    return Array.from({ length: Math.ceil(array.length / chunkSize) })
+      .map((_, index) => array.slice(index * chunkSize, (index + 1) * chunkSize));
+  }
+
   return (
     <React.Fragment>
-      <div className="book-list">
+       <Container>
         {!_.isEmpty(books) ? (
-          books.map((book) => (
-            <Book key={book.id} {...book} handleRemoveBook={handleRemoveBook} />
+          splitArrayIntoChunks(books).map((chunk) => (
+            <Row>
+              { chunk.map((book) => (
+                <Col className="mt-3">
+                  <Book key={book.id} {...book} handleRemoveBook={handleRemoveBook} />
+                </Col>
+              )) }
+            </Row>
           ))
         ) : (
           <p className="message">Nici o carte disponibila. Poti adauga niste carti.</p>
         )}
-      </div>
+      </Container>
     </React.Fragment>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
 const Header = () => {
   return (
-    <header>
-      <h1>Carti Online</h1>
-      <hr />
-      <div className="links">
-        <NavLink to="/" className="link" activeClassName="active" exact>
-          Lista Carti
-        </NavLink>
-        <NavLink to="/add" className="link" activeClassName="active">
-          Adauga Carte
-        </NavLink>
-      </div>
-    </header>
+  <Navbar expand="lg" className="bg-body-secondary">
+  <Container>
+    <Navbar.Brand href="/">Carti Online</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link>
+          <NavLink to="/" className="link" activeClassName="active" exact>
+            Lista Carti
+          </NavLink>
+        </Nav.Link>
+        <Nav.Link>
+          <NavLink to="/adauga" className="link" activeClassName="active">
+            Adauga Carte
+          </NavLink>
+        </Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+  </Navbar>
   );
 };
 
